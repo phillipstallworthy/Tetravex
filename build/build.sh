fd3494afee4c88212323ca26f33fd7287d889232
Phillip Stallworthy <phillipstallworthy@yahoo.com>
2011-04-20 16:05:50 +1200

    initial version

---
+++ build/build.sh
@@ -0,0 +1,2 @@
+cd ../WebContent/dojo-release-1.6.0-src/util/buildscripts
+./build.sh profileFile=../../../../build/profiles/tetravex.profile.js action=clean,release releaseDir=../../../release optimize=shrinksafe
---
+++ build/profiles/tetravex.profile.js
@@ -0,0 +1,18 @@
+dependencies = {
+  layers: [
+    {
+      // This is a specially named layer, literally 'dojo.js'
+      // adding dependencies to this layer will include the modules
+      // in addition to the standard dojo.js base APIs.
+      name: "tetravex.js",
+      dependencies: [
+        "games.Tetravex"
+      ]
+    }
+  ],
+
+  prefixes: [
+    [ "games", "../../games" ],
+    [ "dojox", "../dojox" ]
+  ]
+};
